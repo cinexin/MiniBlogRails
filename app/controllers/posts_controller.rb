e219444a4d028426d89a5b8811989096c97f2659
Migue Guillamón <migueguillamon@gmail.com>
2014-03-26 18:47:36 +0100

    Basic post management finished

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,14 +1,29 @@
 class PostsController < ApplicationController
 
 	def new
+		@post = Post.new
 
+		respond_to do |format|
+			format.html #new.html.erb
+			format.json {render :json => @post}
+		end
 	end
 
 	def create
 		@post = Post.new(post_params)
-		@post.save
+		
 
-		redirect_to @post
+		respond_to do |format|
+			if @post.save
+				format.html {redirect_to @post}	
+				format.json {render :json => @post, :status=>:created, :location=>@post}
+			else
+				format.html {render :action=>:new}				
+				format.json {render :json=>@post.errors, :status=>:unprocessable_entity}
+
+			end
+		end
+		
 	end
 
 	def show
@@ -18,8 +33,33 @@ class PostsController < ApplicationController
 
 	def index
 		@posts = Post.all
+		respond_to do |format|
+			format.html #index.html.erb
+			format.json {render :json=>@posts}
+		end
 	end
 
+	def edit
+		@post = Post.find(params[:id])
+	end
+
+	def update
+		@post = Post.find(params[:id]) 
+		if @post.update(params[:post].permit(:title, :text))
+    		redirect_to @post
+  		else
+    		render 'edit'
+  		end
+	end
+
+	def destroy
+		@post = Post.find(params[:id])
+		@post.destroy
+ 
+		redirect_to posts_path
+	end
+
+	
 	private
 	def post_params
 		params.require(:post).permit(:title,:text)
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,5 @@
 class Post < ActiveRecord::Base
+	validates :text, :presence =>true
+	validates :title, :presence=>true,:length=>{:minimum=>5}
 end
+
